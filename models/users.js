d63bb793dfe9e17535a025091847c9dedf3de599
Isa <iruizord@gmail.com>
2023-08-08 02:16:40 -0600

    added the required guidelines for the users and emails models

---
+++ models/users.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const usersSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        match: [/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please enter a valid email'],
+    },
+})
