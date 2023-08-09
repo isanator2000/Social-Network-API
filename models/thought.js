a434c46786ef36956f3e827a03c1cc2c5746a627
Isa <iruizord@gmail.com>
2023-08-08 18:12:04 -0600

    added thoughtText, username, createdAt, reactions, and virtuals

--- models/thought.js
+++ models/thought.js
@@ -7,4 +7,25 @@ const thoughtSchema = new Schema({
         minlength: 1,
         maxlength: 280,
       },
-})
+
+    username: {
+        type: String, 
+        required: true
+      },
+
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dateFormat(timestamp),
+    },
+    
+    reactions:[reactionSchema]
+    },
+
+    {
+    timestamps: true,
+    toJSON: {
+        virtuals: true,
+    },
+    id: false,
+});
