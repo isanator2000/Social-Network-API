aa42bfc344d7f55ada15d4ae4aaa82c5e31c5db1
Isa <iruizord@gmail.com>
2023-08-10 13:41:25 -0600

    added thought creation and modification functionality

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -24,5 +24,37 @@ async function getThought(req, res) {
     }
 }
 
+async function createNewThought(req, res) {
+    try {
+        const newThought = await Thought.create(req.body);
+        const user = await User.findOneAndUpdate(
+            { _id: req.body.userID },
+            { $push: { thoughts: newThought._id } },
+            { new: true }
+        );
+        res.json(user);
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json(err);
+    }
+}
+
+async function modifyThought(req, res) {
+    try {
+        const updatedThought = await Thought.findOneAndUpdate(
+            { _id: req.params.thoughtID },
+            { $set: req.body },
+            { new: true }
+        );
+        if (!updatedThought) {
+            return res.status(404).json({ message: 'Update failed' });
+        }
+        res.json(updatedThought);
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json(err);
+    }
+}
+
 
 
