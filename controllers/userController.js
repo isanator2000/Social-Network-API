4bd90061d07065ca92405fa3e7d14951d6b7953a
Isa <iruizord@gmail.com>
2023-08-10 10:57:14 -0600

    implementing getUsers route handler

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,2 +1,12 @@
 const { ObjectId } = require('mongoose').Types;
 const { User, Thought } = require('../models');
+
+async function getUsers(req, res) {
+    try {
+      const users = await User.find().populate('thoughts');
+      res.json(users);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  }
