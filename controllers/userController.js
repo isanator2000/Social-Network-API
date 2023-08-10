13a90a429d41859cb5766f233ff67d06b81ba1f8
Isa <iruizord@gmail.com>
2023-08-10 11:16:53 -0600

    adding route handler for single user

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,3 +10,18 @@ async function getUsers(req, res) {
       return res.status(500).json(err);
     }
   }
+
+  async function getSingleUser(req, res) {
+    try {
+      const user = await User.findOne({ _id: req.params.userID }).populate('thoughts');
+      if (!user) {
+        return res.status(404).json({message: 'User not found with this ID'});
+      }
+      res.json(user);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  }
+
+  
