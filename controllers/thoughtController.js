c00bb5774b9f0530df7c016af5a024d136e79220
Isa <iruizord@gmail.com>
2023-08-10 12:56:33 -0600

    added code for fetching list of thoughts and retrieving individual thoughts based on ids

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,3 +1,28 @@
 const { ObjectId } = require('mongoose').Types;
 const { User, Thought } = require('../models');
 
+async function fetchThoughts(req, res) {
+    try {
+        const thoughts = await Thought.find();
+        res.json(thoughts);
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json(err);
+    }
+}
+
+async function getThought(req, res) {
+    try {
+        const thought = await Thought.findOne({_id: req.params.thoughtID});
+        if (!thought) {
+            return res.status(404).json({message: 'Thought not found'});
+        }
+        res.json(thought);
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json(err);
+    }
+}
+
+
+
